b2268984ccdd77142e9b4e6fa75e8a6a94d92f04
wanghuajian620 <154659041@qq.com>
2018-07-23 13:56:31 +0800

    练习 go  example

---
+++ exercise/values.go
@@ -0,0 +1,19 @@
+/*
+ *  Revision History:
+ *      Initial: 2018/07/23    Wang Huajian
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
