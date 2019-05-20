849eff7a53caba6649eb9669bd1828fe06d31f80
wanghuajian620 <154659041@qq.com>
2019-05-20 17:02:25 +0800

    go，I'm comming again

---
+++ restart/hello.go
@@ -0,0 +1,12 @@
+/**
+Revision  History:
+    Initial:  2019/05/20     Author:  Wang Huajian
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello World or 你好，世界 or καλημ ́ρα κóσμ or こんにちはせかい\n")
+}
