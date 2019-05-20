d585323eed5d35cc2cd38e255cf0a511072fceae
wanghuajian620 <154659041@qq.com>
2019-05-20 16:59:27 +0800

    web.go

---
+++ web/web.go
@@ -0,0 +1,14 @@
+/*
+ *  Revision History:
+ *      Initial: 2018/09/13    Wang Huajian
+ */
+
+package main
+
+import (
+	"net/http"
+)
+
+func sayHelloName(w http.ResponseWriter, r *http.Request) {
+
+}
